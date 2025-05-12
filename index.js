d25b9b706fbd3384dd8d5fac95300a928a213d77
darkwolf <jorginhof123@outlook.com>
2025-05-12 16:47:14 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+
+app.get('/proxy', async (req, res) => {
+  const imageUrl = req.query.url;
+  if (!imageUrl) return res.status(400).send('Missing image URL');
+
+  try {
+    const response = await fetch(imageUrl);
+    const contentType = response.headers.get('content-type');
+    const buffer = await response.buffer();
+
+    res.set('Content-Type', contentType);
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send(buffer);
+  } catch (err) {
+    res.status(500).send('Erro ao buscar imagem');
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Proxy rodando');
+});
